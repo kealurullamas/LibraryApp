319864e8a4ab153fe7f64c3eaa5c65bf72ba7d72
gabdelgado <gabgrietadelgado@gmail.com>
2018-02-08 15:56:35 +0800

    search and about us css, no footer yet

--- resources/views/Pages/about.blade.php
+++ resources/views/Pages/about.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="container">
         <h1>About SHL-ARC</h1>
-        <h4>
+        <h4 class="description">
             Sotero H. Laurel - Academic Resource Center
             The Library was named after the late Senator Sotero H.Laurel on September 28,1989
             in abiding appreciation to his passion for scholarship,spirit,of enlighted nationalism,
@@ -14,13 +14,13 @@
         </h4>
 
         <h1>Library Mission</h1>
-        <h4>
+        <h4 class="description">
             To support the University instructional, curricular, research and extension programs
             through an organized, relevant, and fast delivery of information services.            
         </h4>
 
         <h1>General Objectives</h1>
-        <h4>
+        <h4 class="description">
             To provide an adequate, stimulating and collaborative learning environment through
             access to informational sources necessary to keep Lyceum of the Philippines University
             a learning organization.                       
--- resources/views/Pages/index.blade.php
+++ resources/views/Pages/index.blade.php
@@ -2,11 +2,11 @@
 
 @section('content')
     <div class="container">
-        <h1>Welcome to Sotero H. Laurel Academic Resource Center</h1>
+        <h1 class="title">Sotero H. Laurel Academic Resource Center</h1>
         {!!Form::open(['action'=>"BooksController@search",'method'=>'POST'])!!}
-        {{Form::label('title','Search A book')}}
-        {{Form::text('book','',[])}}
-        {{Form::submit('submit',['class'=>'btn btn-primary'])}}
+        <!--{{Form::label('title','Search A book')}} -->
+        {{Form::text('book','',['placeholder'=>'Search for a book or author...', 'class'=>'searchBox'])}}
+        {{Form::submit('Search',['class'=>'btn btn-primary'])}}
         {!!Form::close()!!} 
     </div>
     
